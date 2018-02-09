a9ab4332aafd2202e2c6ab78ed5fa1bc9ff090ee
SAMUEL LINKON GUEDES FIGUEIREDO <35925156+samuellinkon30@users.noreply.github.com>
2018-02-09 21:09:13 -0200

    FINAL CHANGES

--- routes/loginroutes.js
+++ routes/loginroutes.js
@@ -1,4 +1,4 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -6,48 +6,30 @@ var connection = mysql.createConnection({
   database : 'banco_dados'
 });
 connection.connect(function(err){
-if(!err) {
-    console.log("Database is connected");
-} else {
-    console.log("Error connecting database");
-}
+  if(!err) {
+    console.log("Conectado com o DataBase");
+  } else {
+    console.log("Erro na conexao com o DataBase");
+  }
 });
 
 exports.register = function(req,res){
-  // console.log("req",req.body);
   var today = new Date();
   var users={
     "name":req.body.first_name,
     "email":req.body.email,
     "password":req.body.password,
-    
   }
-
- var email= req.body.email;
+  var email= req.body.email;
   var password = req.body.password;
-
-
   connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
-  if (results[0].email == email) {
-    // console.log("error ocurred",error);
-    res.send({
-      "code":400,
-      "failed":"email ja cadastrado"
+    if (results[0].email == email){
+      res.send({
+        "code":400,
+        "failed":"emal alredy register"
     })
-  }
-  else
-  {
-
-  connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
-  if (results[0].email == email){
-    // console.log("error ocurred",error);
-    res.send({
-      "code":400,
-      "failed":"emal alredy register"
-    })
-  } else
-  {
-   connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
+  } else {
+     connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
       if (error) {
         console.log("error ocurred",error);
         res.send({
@@ -59,47 +41,43 @@ exports.register = function(req,res){
         res.send({
           "code":200,
           "success":"user registered sucessfully"
-            });
+        });
       }
-      }); 
+    }); 
   }
+  });
+}
 
-   });
-}
-});
-}
 exports.login = function(req,res){
   var email= req.body.email;
   var password = req.body.password;
   connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
-  if (error) {
-    // console.log("error ocurred",error);
-    res.send({
-      "code":400,
-      "failed":"error ocurred"
-    })
-  }else{
-    // console.log('The solution is: ', results);
+    if (error) {
+      res.send({
+        "code":400,
+        "failed":"error ocurred"
+      })
+  } else {
     if(results.length >0){
       if(results[0].password == password){
         res.send({
           "code":200,
           "success":"login sucessfull"
-            });
+        });
       }
-      else{
+      else {
         res.send({
           "code":204,
           "success":"Email and password does not match"
-            });
+        });
       }
     }
-    else{
+    else {
       res.send({
         "code":204,
         "success":"Email does not exits"
-          });
+      });
     }
   }
-  });
+});
 }
