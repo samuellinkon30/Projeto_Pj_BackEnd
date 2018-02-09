9e0f8c26fbdc7238c576c0d5f56e4c660e287c3e
SAMUEL LINKON GUEDES FIGUEIREDO <35925156+samuellinkon30@users.noreply.github.com>
2018-02-09 21:08:35 -0200

    FINAL CHANGES

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-FINAL CHANGES
+var express = require("express"); 
+var login = require('./routes/loginroutes'); 
+var bodyParser = require('body-parser'); 
+var app = express(); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.json()); 
+  app.use(function(req, res, next) { 
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
+  next(); 
+}); 
+var router = express.Router(); 
+router.post('/register',login.register); 
+router.post('/login',login.login) 
+app.use('/api', router); 
+app.listen(1500); 
